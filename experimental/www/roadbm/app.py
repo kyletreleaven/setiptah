910387edefd65a05e2b17a5cbcd58bebce1337c5
Kyle Treleaven <ktreleav@gmail.com>
2017-02-20 23:02:56 -0500

    spec'ing out the json schema for a roadbm query

--- experimental/www/roadbm/app.py
+++ experimental/www/roadbm/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    if False:
+    	return 'Hello, World!'
+    else:
+    	data = [(0,1)]
+    	return jsonify(data)
+
+if __name__ == "__main__":
+	app.run()
---
+++ experimental/www/roadbm/roadbm_web.py
@@ -0,0 +1,87 @@
+
+import networkx as nx
+
+import requests
+
+import setiptah.roadbm.bm as bm
+
+
+class RoadMatchInstance(object):
+
+	@classmethod
+	def from_json(cls, json):
+		i = cls()
+
+		roads = json['roads']
+
+		roadmap = nx.MultiDiGraph()
+		for road in roads:
+			roadmap.add_edge(road['source'], road['target'], key=road['name'], length=road.get('length'))
+
+		i._roadmap = roadmap
+		i._source_points = [ (p.get('road'), p.get('coordinate')) for p in json.get('source_points') ]
+		i._target_points = [ (p.get('road'), p.get('coordinate')) for p in json.get('target_points') ]
+
+		return i
+
+	def json(self):
+		edges = self._roadmap.edges_iter(keys=True, data=True)
+
+		return {
+			'roads': [{
+				'name': edge,
+				'source': i,
+				'target': j,
+				'length': data.get('length'),
+			} for i, j, edge, data in edges ],
+
+			'source_points': [{
+				'road': road,
+				'coordinate': coord,
+			} for (road, coord) in self._source_points ],
+			'target_points': [{
+				'road': road,
+				'coordinate': coord,
+			} for (road, coord) in self._target_points ],
+		}
+
+	@classmethod
+	def from_args(cls, S, T, roadmap):
+		i = cls()
+		i._source_points = [ p for p in S ]
+		i._target_points = [ p for p in T ]
+		i._roadmap = nx.MultiDiGraph(roadmap)
+
+		return i
+
+	def source_points(self):
+		return [ p for p in self._source_points ]
+
+	def target_points(self):
+		return [ p for p in self._target_points ]
+
+	def get_roadmap(self):
+		return nx.MultiDiGraph(self._roadmap)
+
+
+
+if __name__ == "__main__":
+
+	url = 'http://localhost:5000'
+	#requests.get(url,
+
+	roadmap = nx.MultiDiGraph()
+	roadmap.add_edge(0, 1, 'road1', length=1.)
+
+	S = [ ('road1', .5) ]
+	T = [ ('road1', .75) ]
+
+	inst1 = RoadMatchInstance.from_args(S, T, roadmap)
+
+	json1 = inst1.json()
+
+	inst2 = RoadMatchInstance.from_json(json1)
+
+	json2 = inst2.json()
+
+	bm.ROADSBIPARTITEMATCH(S, T, roadmap)

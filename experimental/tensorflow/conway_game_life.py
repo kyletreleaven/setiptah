6f914bbb66baf44f1d27882ca9a1532562876d28
Kyle Treleaven <ktreleav@gmail.com>
2017-05-06 09:01:56 -0400

    more wip NN for batched conway game of life

--- experimental/tensorflow/conway_game_life.py
+++ experimental/tensorflow/conway_game_life.py
@@ -3,6 +3,9 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
+# TODO: Train a network to predict CGoL.
+# TODO: Write it as a custom Estimator.
+
 
 def conway_next_world(world):
 	# over numpy arrays, for py_func
@@ -22,17 +25,41 @@ def conway_next_world(world):
 
 
 def input_fn(shape, batch_size):
-	pass
+	m, n = shape
+
+	worlds = tf.random_uniform([batch_size,m,n],0,2,dtype=tf.int32)
+
+	# going to need to shape this...
+	callable = lambda w: tf.py_func(conway_next_world, [w], tf.int32, stateful=False)
+
+	worlds_flat = tf.to_float(tf.contrib.layers.flatten(worlds))
+
+	N = np.prod(shape)
+	layer1 = tf.contrib.layers.fully_connected(worlds_flat, N, weights_initializer=tf.random_normal_initializer())
+	layer2 = tf.contrib.layers.fully_connected(layer1, N, weights_initializer=tf.random_normal_initializer())
+
+	logits = tf.reshape(layer2, [-1] + shape)
+
+	# TODO: Use loss for logistic regression.
+	target_next_worlds = tf.map_fn(callable, worlds)
+
+	loss = tf.losses.sigmoid_cross_entropy(
+		target_next_worlds,
+		logits)
+
+	worlds_ = tf.sigmoid(logits)
+
+	return worlds, worlds_, layer1, layer2, logits, loss
 
 
 if __name__ == '__main__':
 
-	if True:
-		shape = 9,9
+	shape = 9,9
+	m, n = shape
 
+	if False:
 		g = tf.Graph()
 		with g.as_default():
-			m, n = shape
 
 			#worlds = tf.random_uniform([batch_size,m,n],0,2,dtype=tf.int32)
 			world = tf.random_uniform([m,n],0,2,dtype=tf.int32)
@@ -45,3 +72,25 @@ if __name__ == '__main__':
 
 		print world_out
 		print world_out_
+
+
+	if True:
+		g = tf.Graph()
+		with g.as_default():
+
+			worlds, worlds_, layer1, layer2, logits, loss = input_fn([2,4], 20)
+
+			with tf.Session() as sess:
+				sess.run(tf.global_variables_initializer())
+
+				opt = tf.train.AdagradOptimizer(learning_rate=0.001)
+				
+				for k in xrange(100):
+					print k
+					opt.minimize(loss)
+
+				# get samples after learning				
+				res_out = sess.run([worlds,worlds_])
+
+				#worlds_out, worlds_out_ = sess.run([worlds,worlds_])
+

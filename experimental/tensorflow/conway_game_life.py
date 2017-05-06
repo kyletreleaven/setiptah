f7ee06c027508319cdccfcbe8451abfe6f817efb
Kyle Treleaven <ktreleav@gmail.com>
2017-05-06 15:42:25 -0400

    a little punched out on this example; it's silly that it doesn't work

--- experimental/tensorflow/conway_game_life.py
+++ experimental/tensorflow/conway_game_life.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
+# Very frustrating. This is practically identical to example code, but does not converge.
+
 # TODO: Train a network to predict CGoL.
 # TODO: Write it as a custom Estimator.
 
@@ -25,36 +27,67 @@ def conway_next_world(world):
 
 
 def input_fn(shape, batch_size):
+	res = dict()
+
 	m, n = shape
 
-	worlds = tf.random_uniform([batch_size,m,n],0,2,dtype=tf.int32)
+	# don't worry about batch approach for now...
+	#worlds = tf.random_uniform([batch_size,m,n],0,2,dtype=tf.int32)
+	world = tf.random_uniform([m,n],0,2,dtype=tf.int32)
+	next_world = tf.py_func(conway_next_world, [world], tf.int32, stateful=False)
+
+	res['input'] = world, next_world
 
 	# going to need to shape this...
-	callable = lambda w: tf.py_func(conway_next_world, [w], tf.int32, stateful=False)
-
-	worlds_flat = tf.to_float(tf.contrib.layers.flatten(worlds))
-
-	N = np.prod(shape)
-	layer1 = tf.contrib.layers.fully_connected(worlds_flat, N, weights_initializer=tf.random_normal_initializer())
-	layer2 = tf.contrib.layers.fully_connected(layer1, N, weights_initializer=tf.random_normal_initializer())
-
-	logits = tf.reshape(layer2, [-1] + shape)
+	#callable = lambda w: tf.py_func(conway_next_world, [w], tf.int32, stateful=False)
 
 	# TODO: Use loss for logistic regression.
-	target_next_worlds = tf.map_fn(callable, worlds)
+	#target_next_worlds = tf.map_fn(callable, worlds)
 
-	loss = tf.losses.sigmoid_cross_entropy(
-		target_next_worlds,
-		logits)
+	#world_flat = tf.to_float(tf.contrib.layers.flatten(world))
 
-	worlds_ = tf.sigmoid(logits)
+	# The first dimension is because layers treat data in batches, i.e., [batch_size==1,-1]
+	input_layer = tf.to_float(tf.reshape(world,[1,-1]))
 
-	return worlds, worlds_, layer1, layer2, logits, loss
+	N = np.prod(shape)
+	layer1 = tf.contrib.layers.fully_connected(input_layer, N)
+	#weights_initializer=tf.random_normal_initializer())
+	layer2 = tf.contrib.layers.fully_connected(layer1, N)
+	#weights_initializer=tf.random_normal_initializer())
+	
+	# next world statistics
+	res['output'] = outputs = dict()
+
+	if True:
+		layer3 = tf.contrib.layers.fully_connected(layer2, N,
+			activation_fn=tf.nn.sigmoid)
+
+		next_world_probabilities = tf.reshape(layer3, shape)
+
+	else:
+		layer3 = tf.contrib.layers.fully_connected(layer2, N, activation_fn=None) # stay linear
+		logits = tf.reshape(layer3, shape)
+
+		outputs['logits'] = logits
+
+		next_world_probabilities = tf.sigmoid(logits)
+
+	next_world_prediction = tf.to_int32(tf.greater(next_world_probabilities,.5))
+
+	outputs['next_world_probabilities'] = next_world_probabilities
+	outputs['next_world_prediction'] = next_world_prediction
+
+	#loss = tf.losses.sigmoid_cross_entropy(next_world, logits)
+	#loss = tf.losses.log_loss(next_world, next_world_probabilities)
+	loss = tf.losses.log_loss(next_world_probabilities, next_world)
+	res['loss'] = loss
+
+	return res
 
 
 if __name__ == '__main__':
 
-	shape = 9,9
+	shape = 5,5
 	m, n = shape
 
 	if False:
@@ -75,22 +108,40 @@ if __name__ == '__main__':
 
 
 	if True:
+		losses = []
+
 		g = tf.Graph()
 		with g.as_default():
 
-			worlds, worlds_, layer1, layer2, logits, loss = input_fn([2,4], 20)
+			ops = input_fn(shape, None) # no batch size currently
+
+			loss = ops.get('loss')
+			optimizer = tf.train.AdagradOptimizer(1.0)
+			train = optimizer.minimize(loss)
 
 			with tf.Session() as sess:
 				sess.run(tf.global_variables_initializer())
 
-				opt = tf.train.AdagradOptimizer(learning_rate=0.001)
-				
-				for k in xrange(100):
-					print k
-					opt.minimize(loss)
+				for k in xrange(10000):
+					_, loss_out = sess.run([train, loss])
 
-				# get samples after learning				
-				res_out = sess.run([worlds,worlds_])
+					if k % 100 == 0 :
+						print k, loss_out
+						losses.append(loss_out)
 
-				#worlds_out, worlds_out_ = sess.run([worlds,worlds_])
+				# get samples after learning
+				world_op, _ = ops['input']
+				next_world_prediction_op = ops['output'].get('next_world_prediction')
 
+				for k in xrange(20):
+					world, next_world_predicted = sess.run([world_op, next_world_prediction_op])
+
+					next_world = conway_next_world(world)
+
+					print np.sum(np.sum(np.abs(next_world-next_world_predicted)))
+
+		import matplotlib.pyplot as plt
+		plt.plot(range(len(losses)), losses)
+		plt.show()
+
+		# TODO(ktreleav): Save/load the model.

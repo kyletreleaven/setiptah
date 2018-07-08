ca2748b951e3631cedaf1692817bb3e383524e54
Kyle Treleaven <ktreleav@gmail.com>
2018-07-08 19:45:09 -0400

    adding basic typescript example

---
+++ edu/typescript/greeter.ts
@@ -0,0 +1,23 @@
+/* 
+ * https://www.typescriptlang.org/docs/tutorial.html
+ */
+ 
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
